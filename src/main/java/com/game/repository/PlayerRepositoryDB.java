110a80dc0e888da5d3032dd81bb7e3a78ecf6643
Roman Mudrud <MudrudR@gmail.com>
2024-04-19 19:22:08 +0300

    added dependencies into pom.xml, added annotation into Player.java, added Properties, build SessionFactory, implementation getAll method

--- src/main/java/com/game/repository/PlayerRepositoryDB.java
+++ src/main/java/com/game/repository/PlayerRepositoryDB.java
@@ -1,22 +1,45 @@
 package com.game.repository;
 
 import com.game.entity.Player;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.query.NativeQuery;
 import org.springframework.stereotype.Repository;
 
 import javax.annotation.PreDestroy;
 import java.util.List;
 import java.util.Optional;
+import java.util.Properties;
 
 @Repository(value = "db")
 public class PlayerRepositoryDB implements IPlayerRepository {
+    private final SessionFactory sessionFactory;
 
     public PlayerRepositoryDB() {
+        Properties prop = new Properties();
+        prop.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
+        prop.put(Environment.URL, "jdbc:mysql://localhost:3306/rpg");
+        prop.put(Environment.USER, "root");
+        prop.put(Environment.PASS, "root");
+        prop.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5InnoDBDialect");
+        prop.put(Environment.HBM2DDL_AUTO, "update");
 
+        sessionFactory = new Configuration()
+                .addAnnotatedClass(Player.class)
+                .addProperties(prop)
+                .buildSessionFactory();
     }
 
     @Override
     public List<Player> getAll(int pageNumber, int pageSize) {
-        return null;
+        try(Session session = sessionFactory.openSession()) {
+            NativeQuery<Player> query = session.createNativeQuery("select * from player", Player.class);
+            query.setFirstResult(pageNumber * pageSize);
+            query.setMaxResults(pageSize);
+            return query.list();
+        }
     }
 
     @Override
